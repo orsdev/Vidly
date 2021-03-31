b4ae19df0c61a64996086f2b4dab445fb88eff83
Ogundare Samuel <oluwadareysamuel@yahoo.com>
2021-03-31 11:27:44 +0100

    Customer model , controller initialized

---
+++ Vidly/Models/Customers.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Vidly.Models
+{
+    public class Customers
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Name { get; set; }
+
+        [Required]
+        public DateTime DOB { get; set; }
+
+    }
+}
