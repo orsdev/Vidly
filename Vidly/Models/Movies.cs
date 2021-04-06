b8b45a3e4c2b7b3df9f4bdbda8fbf8d5c9c35410
Ogundare Samuel <oluwadareysamuel@yahoo.com>
2021-04-06 09:21:54 +0100

    annotations added to movies model

--- Vidly/Models/Movies.cs
+++ Vidly/Models/Movies.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,14 +9,23 @@ namespace Vidly.Models
     public class Movies
     {
         public int Id { get; set; }
+
+        [Required]
+        [Display(Name = "Movie Title")]
         public string Name { get; set; }
 
+        [Required]
+        [Display(Name = "Release Date")]
         public DateTime ReleaseDate { get; set; }
 
+        [Required]
+        [Display(Name = "Number in stock")]
+        [Range(1, 20)]
         public int NumberInStock { get; set; }
 
         public Genres Genre { get; set; }
 
+        [Required(ErrorMessage = "Select movie genre")]
         public int GenreId { get; set; }
     }
 }
