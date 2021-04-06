5ca36c90f1c882a03c8356dd0c66f407be68cc7c
Ogundare Samuel <oluwadareysamuel@yahoo.com>
2021-04-06 07:30:24 +0100

    Genre model linked to movies model

---
+++ Vidly/Models/Movies.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Vidly.Models
+{
+    public class Movies
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public DateTime ReleaseDate { get; set; }
+
+        public int NumberInStock { get; set; }
+
+        public Genres Genre { get; set; }
+
+        public int GenreId { get; set; }
+    }
+}
