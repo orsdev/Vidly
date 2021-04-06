374d42a31d378764d68b8358bc7659010e7f9c77
Ogundare Samuel <oluwadareysamuel@yahoo.com>
2021-04-06 07:27:01 +0100

    edit & save action added

---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,93 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+using System.Data.Entity;
+using Vidly.Models;
+using Vidly.ViewModels;
+
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        /*
+         *  links the database server to the data model class
+         */
+        private readonly ApplicationDbContext _context;
+
+        public MoviesController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
+
+        public ActionResult Index()
+        {
+            var movies = _context.Movies.Include(c => c.Genre).ToList();
+            return View(movies);
+        }
+
+        public ActionResult New()
+        {
+            var _genre = _context.Genres.ToList();
+
+            var viewModel = new MovieViewModel
+            {
+                Movies = new Movies(),
+                Genres = _genre
+            };
+
+
+            return View("MovieForm", viewModel);
+        }
+
+
+        public ActionResult Edit(int id)
+        {
+            var _movies = _context.Movies.SingleOrDefault(c => c.Id == id);
+            var _genre = _context.Genres.ToList();
+
+            var viewModel = new MovieViewModel
+            {
+                Movies = _movies,
+                Genres = _genre
+            };
+
+
+            return View("MovieForm",viewModel);
+
+        }
+
+        public ActionResult Save(Movies movies)
+        {
+            if (movies.Id == 0)
+            {
+                _context.Movies.Add(movies);
+            }
+            else
+            {
+
+                var DBMovies = _context.Movies.SingleOrDefault(c => c.Id == movies.Id);
+
+                if (DBMovies == null)
+                {
+                    return HttpNotFound();
+                }
+
+                DBMovies.Name = movies.Name;
+                DBMovies.GenreId = movies.GenreId;
+                DBMovies.NumberInStock = movies.NumberInStock;
+                DBMovies.ReleaseDate = movies.ReleaseDate;
+
+            }
+
+            _context.SaveChanges();
+
+            return RedirectToAction("Index", "Movies");
+        }
+    }
+}
