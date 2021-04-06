cee8d6c431a2eb59ed9cfa2a975a4074b0dd2148
Ogundare Samuel <oluwadareysamuel@yahoo.com>
2021-04-06 07:29:57 +0100

    movies & customers view model added

---
+++ Vidly/ViewModels/MovieViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Vidly.Models;
+
+namespace Vidly.ViewModels
+{
+    public class MovieViewModel
+    {
+        public Movies Movies { get; set; }
+        public IEnumerable<Genres> Genres { get; set; }
+    }
+}
