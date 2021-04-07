1bf1bb3599d9a2d3bc41bb6439bf13acc99af4f4
Ogundare Samuel <oluwadareysamuel@yahoo.com>
2021-04-07 09:03:04 +0100

    Title added to CustomerFormViewModel

--- Vidly/ViewModels/CustomerFormViewModel.cs
+++ Vidly/ViewModels/CustomerFormViewModel.cs
@@ -10,5 +10,15 @@ namespace Vidly.ViewModels
     {
         public Customers Customers { get; set; }
         public IEnumerable<MembershipType> MembershipTypes { get; set; }
+
+        public string Title 
+        { get
+          {
+                if (Customers != null && Customers.Id != 0)
+                    return "Edit Customer";
+                else
+                    return "Add Customer";
+          }
+        }
     }
 }
